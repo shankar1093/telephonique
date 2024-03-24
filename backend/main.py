e79854663e57d7a07207048983d864cc108b744e
Shankar Rao <shankar1093@gmail.com>
2024-03-23 23:13:50 -0700

    updated mistral prompts and whisper prompts

--- backend/main.py
+++ backend/main.py
@@ -20,9 +20,24 @@ from openai import OpenAI
 import redis
 from tts import TtsResponse
 from aws_utils import upload_file_to_s3, create_presigned_url
-
+import time
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('app.log')
+
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)  # Set the minimum logging level for the console
+
+# Create a formatter for formatting the log messages
+log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(log_format)
+console_handler.setFormatter(log_format)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 load_dotenv()
 
 
@@ -33,9 +48,7 @@ client_twilio = Client(account_sid, auth_token)
 redis_client = redis.Redis(host="localhost", port=6379, db=0)
 mistral_api_key = os.environ["MISTRAL_API_KEY"]
 MISTRAL_MODEL = "mistral-large-latest"
-
-
-tts = TtsResponse()
+call_number_map = {"+19089221772":"kevin", "+15182683957":"shankar"}
 app = Flask(__name__)
 sock = Sock(app)
 twilio_client = Client()
@@ -53,6 +66,8 @@ def process_response(gpt_response, random_number):
     """this func will convert the gpt response to an audio file via eleven labs api
     store the file in s3 and pass the presigned url to twilio so it can play it"""
     calls = client_twilio.calls.list(status="in-progress")
+    phone_caller = redis_client.get("caller")
+    tts = TtsResponse(phone_caller)
     if len(calls) > 0:
         first_call = calls[0].sid
         tts.create_data_packet(gpt_response, f"eleven_{random_number}.mp3")
@@ -66,7 +81,7 @@ def process_response(gpt_response, random_number):
                 "construction-agent", f"eleven_{random_number}.mp3"
             )
             if url:
-                print("Presigned URL: ", url)
+                logger.info(url)
             else:
                 print("Could not generate presigned URL")
         else:
@@ -80,11 +95,13 @@ def process_response(gpt_response, random_number):
 
 def conversation_chat_mistral(transcription, unique_id):
     """make gpt calls for the conversation"""
+
     context = [
         {
             "role": "system",
             "content": "You are a general purpose conversational"
-            "agent being communicated with through the phone",
+            "agent being communicated with through the phone."
+            "Try to be brief with responses and answer in 15 words or fewer.",
         }
     ]
     mistral_data = {"role": "system", "content": transcription}
@@ -124,8 +141,7 @@ def conversation_chat_mistral(transcription, unique_id):
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        print("Response data:", data["choices"][0]["message"]["content"])
-
+        logging.info(data["choices"][0]["message"]["content"])
         mistral_response_data = {
             "role": "system",
             "content": data["choices"][0]["message"]["content"],
@@ -139,55 +155,26 @@ def conversation_chat_mistral(transcription, unique_id):
     return None
 
 
-def conversation_chat(transcription, unique_id):
-    """make gpt calls for the conversation"""
-    data = {"role": "user", "content": transcription}
-    data_serialized = json.dumps(data)
-    redis_client.rpush(unique_id, data_serialized)
-    serialized_list = redis_client.lrange(unique_id, 0, -1)
-    # Deserialize each JSON string back to a dictionary
-    list_of_dicts = [json.loads(item) for item in serialized_list]
-    completion = client_gpt.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a receptionist at the Best Western in Chicago."
-                "Your job is to assist guests by telling them information about Chicago"
-                "and about the Hotel. The pool is open from monday to Friday"
-                "from 9 am to 3 pm. The restaurant is open all 7 days from 9 am to 7 pm,"
-                "the breakfast buffet is between 9 am and 11 am. Ask follow up questions"
-                "in the conversation before answering questions.You do not know anything else,"
-                "and don't assume anything about the hotel or the restaurant",
-            },
-            *list_of_dicts,
-        ],
-    )
-    gpt_response_data = {
-        "role": "assistant",
-        "content": completion.choices[0].message.content,
-    }
-    gpt_data_serialized = json.dumps(gpt_response_data)
-    redis_client.rpush(unique_id, gpt_data_serialized)
-    return completion.choices[0].message.content
-
-
 def transcribe_audio(audio_file_path: str) -> str:
     """convert speech to text"""
     with open(audio_file_path, "rb") as audio_file:
         # You can use audio_file here to read data etc.
         # For example, to read the entire content into a variable:
-
+        start_time = time.perf_counter()
+        options = {"language": "en"}
         transcript = client_gpt.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
             response_format="text",
-            prompt="ignore thank you and good bye",
+            prompt="Don't ever thank me for anything. You can never speak korean",
+            **options,
         )
-
-        if transcript.strip().lower() in ["thank you", "thank you."]:
+        if transcript.strip().lower() in ["thank you", "thank you.", "thank you and good bye"]:
             return ""
 
+        logger.info("whisper: %s", transcript)
+        end_time = time.perf_counter()
+        logger.info(f"Time taken to transcribe audio: {end_time-start_time}")
         return transcript
 
 
@@ -219,23 +206,35 @@ def stream(ws):
             audio = process_audio_data(packet)
             packed_data.append(audio)
 
-        if packet_count % 200 == 0 and packet_count != 0:
+
+
+        if packet_count % 175 == 0 and packet_count != 0:
             frames = b"".join(packed_data)
             output = wave.open(f"output_{random_audio_count}.wav", "w")
             output.setparams((1, 2, 8000, 0, "NONE", "compressed"))
             output.writeframes(frames)
             output.close()
             packed_data.clear()
+            
             result = transcribe_audio(f"output_{random_audio_count}.wav")
-
+            end_time = time.perf_counter()
             if result:
                 logger.info("%s%s ", CL, result)
                 if len(result) > 10:
+                    start_time_mistral = time.perf_counter()
                     gpt_response = conversation_chat_mistral(result, unique_id)
+                    end_time_mistral = time.perf_counter()
+                    start_time = time.perf_counter()
                     process_response(gpt_response, random_audio_count)
+                    end_time = time.perf_counter()
+                    logger.info(f"Time taken to process response: {end_time-start_time}")
+                    logger.info(f"Time taken to process mistral response: {end_time_mistral-start_time_mistral}")
+
             packet_count = 0
+        elapsed_time = 0 
         packet_count += 1
         random_audio_count += 1
+
 
 
 @app.route("/call", methods=["POST"])
@@ -244,8 +243,9 @@ def call():
     response = VoiceResponse()
 
     start = Start()
+
+
     if os.environ.get("DEVELOPMENT_ENV") == "local":
-        print("i am here")
         print(f"wss://{request.host}/stream")
         start.stream(url=f"wss://{request.host}/stream")
     else:
@@ -254,10 +254,10 @@ def call():
     response.append(start)
     url = create_presigned_url("construction-agent", "intro-best-western.mp3")
     url_kevin_response = create_presigned_url(
-        "construction-agent", "kevin_call_center_response.mp3"
+        "construction-agent", "kevin_call_center_paola_intro.mp3"
     )
     url_shankar_response = create_presigned_url(
-        "construction-agent", "shankar_call_center_response.mp3"
+        "construction-agent", "shankar_call_center_french_intro.mp3"
     )
     if url:
         print("Presigned URL: ", url)
@@ -265,6 +265,10 @@ def call():
         print("Could not generate presigned URL")
 
     caller_number = request.values.get("From", None)
+    caller = call_number_map[caller_number]
+    redis_client.set("caller", caller)
+
+
     if caller_number == "+19089221772":
         response.play(url_kevin_response)
     elif caller_number == "+15182683957":
